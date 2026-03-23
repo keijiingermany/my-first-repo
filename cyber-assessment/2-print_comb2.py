3098e2c87a69475e6980222c37112f02e2b8bb65
Keiji Uehara <keijiingermany@gmail.com>
2026-03-23 16:39:57 +0100

    Refactor print_comb2.py for concise output

--- cyber-assessment/2-print_comb2.py
+++ cyber-assessment/2-print_comb2.py
@@ -1,7 +1,2 @@
 #!/usr/bin/python3
-
-for i in range(100):
-    if i == 99:
-        print(f"{i:02}")
-    else:
-        print(f"{i:02}", end=", ")
+for i in range(100): print("%02d" % i, end=", " if i < 99 else "\n")
